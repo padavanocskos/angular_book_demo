d5f89e2caca703df27e4303a17a413a65519b202
padavanocskos <davidbecsei@gmail.com>
2023-09-29 23:24:56 +0200

    Components list

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -7,37 +7,40 @@ import { Stock } from 'src/app/model/stock';
   styleUrls: ['./stock-item.component.css'],
 })
 export class StockItemComponent implements OnInit {
-  public stock: Stock;
-  public stockClasses: {
-    positive: boolean;
-    negative: boolean;
-    'large-change': boolean;
-    'small-change': boolean;
-  };
-
+  public stocks: Stock[];
   constructor() {}
 
   ngOnInit() {
-    this.stock = new Stock(
-      'Stock ITEM',
-      'TSC',
-      80,
-      90,
-      'https://picsum.photos/id/21/200',
-      10
-    );
-    let diff = this.stock.price / this.stock.previousPrice - 1;
-    let largeChange = Math.abs(diff) > 0.05;
-    this.stockClasses = {
-      positive: !this.stock.isPositiveChange(),
-      negative: this.stock.isPositiveChange(),
-      'large-change': largeChange,
-      'small-change': !largeChange,
-    };
+    this.stocks = [
+      new Stock(
+        'Stock ITEM',
+        'TSC',
+        80,
+        90,
+        'https://picsum.photos/id/21/200',
+        10
+      ),
+      new Stock(
+        'Stock ITEM 2',
+        'TSC',
+        80,
+        90,
+        'https://picsum.photos/id/20/200',
+        10
+      ),
+      new Stock(
+        'Stock ITEM 3',
+        'TSC',
+        80,
+        90,
+        'https://picsum.photos/id/22/200',
+        10
+      ),
+    ];
   }
 
-  toggleFavorite(event: MouseEvent) {
+  toggleFavorite(event: MouseEvent, index: number) {
     console.log('This is an event click', event);
-    this.stock.favorite = !this.stock.favorite;
+    this.stocks[index].favorite = !this.stocks[index].favorite;
   }
 }
